4c77be82476a2500433c382c52bd57cf3c3b52e7
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2024-08-10 19:30:32 +0530

    dashboard built

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTableCreator, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const createTable = pgTableCreator(
@@ -11,6 +12,29 @@ export const merchant = createTable("merchant", {
   number: varchar("number").unique(),
   password: varchar("password", { length: 255 }).notNull(),
 });
+
+export const merchantRelations = relations(merchant, ({ many }) => ({
+  link: many(link),
+}));
+
+export const link = createTable("link", {
+  id: varchar("id", { length: 21 }).primaryKey(),
+  merchantId: varchar("merchant_id", { length: 21 })
+    .notNull()
+    .references(() => merchant.id),
+  url: varchar("url", { length: 255 }).notNull(),
+});
+
+export type Link = typeof link.$inferSelect;
+export type NewLink = typeof link.$inferInsert;
+
+export const linkRelations = relations(link, ({ one }) => ({
+  merchant: one(merchant, {
+    fields: [link.merchantId],
+    references: [merchant.id],
+  }),
+}))
+  ;
 
 export type Merchant = typeof merchant.$inferSelect;
 export type NewMerchant = typeof merchant.$inferInsert;

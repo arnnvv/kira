747315a286ee758e7ab50e1f959efbaca907108e
arnnvv <arnav.900@protonmail.com>
2024-08-11 19:10:31 +0530

    kira

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { validateRequest } from "@/actions";
 import { Homepage } from "@/components/ui/homepage";
 import { db } from "@/lib/db";
 import { merchant } from "@/lib/db/schema";
+import { redirect } from "next/navigation";
 
 export default async function Home(): Promise<JSX.Element> {
+  const { user } = await validateRequest();
+  if (user) return redirect(`/dashboard/${user.id}`);
   const merchants = await db.select().from(merchant);
 
   return <Homepage merchants={merchants} />;

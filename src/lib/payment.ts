8e084014187c6cf23e97829114e1c0adcfa633b4
arnnvv <arnav.900@protonmail.com>
2024-08-11 15:05:35 +0530

    singleton

--- src/lib/payment.ts
+++ src/lib/payment.ts
@@ -1,22 +1,38 @@
 import Razorpay from "razorpay";
 
-const getRazorpay = (): { key_id: string; key_secret: string } => {
-  const razorpayKeyId: string | undefined = process.env.RAZORPAY_KEY_ID;
-  const razorpayKeySecret: string | undefined = process.env.RAZORPAY_KEY_SECRET;
+class RazorpayInstance {
+  private static instance: RazorpayInstance;
+  private razorpay: Razorpay;
 
-  if (!razorpayKeyId || razorpayKeyId.length === 0)
-    throw new Error("Razorpay key id not found");
+  private constructor() {
+    const { key_id, key_secret } = this.getRazorpayCredentials();
+    this.razorpay = new Razorpay({ key_id, key_secret });
+  }
 
-  if (!razorpayKeySecret || razorpayKeySecret.length === 0)
-    throw new Error("Razorpay key secret not found");
+  public static getInstance(): RazorpayInstance {
+    if (!this.instance) this.instance = new RazorpayInstance();
+    return this.instance;
+  }
 
-  return {
-    key_id: razorpayKeyId,
-    key_secret: razorpayKeySecret,
-  };
-};
+  private getRazorpayCredentials(): { key_id: string; key_secret: string } {
+    const razorpayKeyId: string | undefined = process.env.RAZORPAY_KEY_ID;
+    const razorpayKeySecret: string | undefined =
+      process.env.RAZORPAY_KEY_SECRET;
 
-export const razorpay = new Razorpay({
-  key_id: getRazorpay().key_id,
-  key_secret: getRazorpay().key_secret,
-});
+    if (!razorpayKeyId || razorpayKeyId.length === 0)
+      throw new Error("Razorpay key id not found");
+    if (!razorpayKeySecret || razorpayKeySecret.length === 0)
+      throw new Error("Razorpay key secret not found");
+
+    return {
+      key_id: razorpayKeyId,
+      key_secret: razorpayKeySecret,
+    };
+  }
+
+  public getRazorpay(): Razorpay {
+    return this.razorpay;
+  }
+}
+
+export const razorpay: Razorpay = RazorpayInstance.getInstance().getRazorpay();
